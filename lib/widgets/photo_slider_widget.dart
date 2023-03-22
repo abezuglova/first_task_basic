51eeb614b5e98c32db7dd9a19a2ac130c4959678
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-03-22 13:50:34 +0300

    slider done

--- lib/widgets/photo_slider_widget.dart
+++ lib/widgets/photo_slider_widget.dart
@@ -9,37 +9,55 @@ class PhotoSliderWidget extends StatefulWidget {
 }
 
 class _PhotoSliderWidgetState extends State<PhotoSliderWidget> {
+  static const imagesList = [
+    Image(image: AssetImage(AppImages.firstImage)),
+    Image(image: AssetImage(AppImages.firstImage)),
+    Image(image: AssetImage(AppImages.firstImage)),
+    Image(image: AssetImage(AppImages.firstImage)),
+    Image(image: AssetImage(AppImages.firstImage)),
+  ];
+
+  final controller = PageController();
+  int index = 0;
+  
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return Padding(
+    return Container(
+      width: 346,
+      height: 235,
       padding: const EdgeInsets.fromLTRB(15, 0, 14, 0),
-      child: Center(
-        child: Stack(
-          children: [
-            const Image(
-              image: AssetImage(AppImages.firstImage),
-              width: 346,
-              height: 235,
-            ),
-            Positioned(
-              left: 162,
-              bottom: 10,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 3),
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.6),
-                  borderRadius: const BorderRadius.all(Radius.circular(4)),
-                ),
-                child: Text(
-                  '1/5',
-                  style: textTheme.headlineMedium!.copyWith(color: Colors.white),
-                ),
+      child: Stack(
+        children: [
+          PageView(
+            controller: controller,
+            onPageChanged: (value) => setState(() {
+              index = value;
+            }),
+            children: imagesList,
+          ),
+          Positioned(
+            left: 162,
+            bottom: 10,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 3),
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.6),
+                borderRadius: const BorderRadius.all(Radius.circular(4)),
+              ),
+              child: Text(
+                '${index + 1}/${imagesList.length}',
+                style: textTheme.headlineMedium!.copyWith(color: Colors.white),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
